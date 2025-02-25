c0eb1969ed21a8349130a05b38be5d0e206ff924
tewarig <gauravtewari111@gmail.com>
2025-02-25 22:54:53 +0530

    feat: add charka ui

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,17 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Provider } from "@/components/ui/provider"
 import './index.css'
 import App from './App.tsx'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+<StrictMode>
+  <Provider>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </Provider>
+ </StrictMode>,
 )
